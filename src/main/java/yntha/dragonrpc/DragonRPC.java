e66ae325a4d85bc2608fd754c0fe6f4936a988d1
yntha <126660548+yntha@users.noreply.github.com>
2023-12-14 16:53:07 -0500

    Add Discord GameSDK libs

---
+++ src/main/java/yntha/dragonrpc/DragonRPC.java
@@ -0,0 +1,22 @@
+package yntha.dragonrpc;
+
+import ghidra.app.plugin.ProgramPlugin;
+import ghidra.framework.plugintool.PluginEvent;
+import ghidra.framework.plugintool.PluginTool;
+
+public class DragonRPC extends ProgramPlugin {
+
+    public DragonRPC(PluginTool plugintool) {
+        super(plugintool);
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+    }
+
+    @Override
+    public void processEvent(PluginEvent event) {
+        super.processEvent(event);
+    }
+}
